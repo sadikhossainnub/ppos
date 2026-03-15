9db8228b5e86ec171f4690cfb9e6086f9bbecca6
sadikhossainnub <sadik.hossain.nub@gmail.com>
2026-03-16 02:33:13 +0600

    feat: Add core POS application API for managing opening shifts and retrieving item data.

--- ppos/ppos/api/posapp.py
+++ ppos/ppos/api/posapp.py
@@ -15,7 +15,6 @@ from erpnext.accounts.party import get_party_bank_account
 from erpnext.stock.doctype.batch.batch import (
     get_batch_no,
     get_batch_qty,
-    set_batch_nos,
 )
 from erpnext.accounts.doctype.payment_request.payment_request import (
     get_dummy_message,
@@ -620,7 +619,7 @@ def submit_invoice(invoice, data):
     payments = invoice_doc.payments
 
     if frappe.get_value("POS Profile", invoice_doc.pos_profile, "ppos_auto_set_batch"):
-        set_batch_nos(invoice_doc, "warehouse", throw=True)
+        invoice_doc.set_batch_nos()
     set_batch_nos_for_bundels(invoice_doc, "warehouse", throw=True)
 
     invoice_doc.flags.ignore_permissions = True
